8a05c7bf68ea48946fda364f07a9e960bc9ce18e
Unknown2805 <nuraliali2805@gmail.com>
2024-01-10 12:39:08 +0700

    menambahkan soal UAS semester 1 2023/2024 Reguler Pagi

---
+++ learning/uas/1.pas
@@ -0,0 +1,74 @@
+    // 1. Code ini tidak di desain untuk menjalankan 2 program sekaligus
+    // 2. Uncomment code yang ingin digunakan, comment kembali code yang sudah digunakan
+program uas_1;
+uses crt;
+var 
+a1,b1:real;
+a,b,c,d,e,i:integer;
+baris,kolom:integer;
+total:integer;
+begin
+clrscr;
+
+    // no 1
+        // write('masukkan tabungan awal: ');readln(a1);
+        // write('masukkan suku bunga: ');readln(b1);
+        // write('masukkan jumlah bulan: ');readln(c);
+
+        // d := 1;
+        // repeat
+        //         writeln('bulan ke-',d,' : ',a1:2:0);
+        //         a1 := a1 + a1*b1/100;
+        //         Inc(d);
+
+        // until(d > c);
+
+    // no 2
+        // write('Bilangan pertama: ');read(a);
+        // write('Rasio: ');read(b);
+        // write('Jumlah bilangan: ');read(c);
+        // write('Deret Bilangan: ');
+        // i:=1;
+        // total:= 0;
+        // while (i <= c) do 
+        //     begin
+        //         write(a,' ');
+        //         a:= a*b;
+        //         total := total + a;
+        //         Inc(i);
+        //     end;
+        // writeln;
+        // writeln('Total: ',total);
+
+    // no_3 
+        // write('Bilangan pertama: ');read(a);
+        // write('Jumlah yang ingin ditampilkan: ');read(b);
+        // write('Deret Bilangan: ');
+        
+        // for i:=1 to b do
+        //     begin
+        //         write(a,' ');
+        //         if(a mod 2 = 0) then
+        //             a:= a div 2
+        //         else
+        //             a := a*3 +1;
+        //     end;
+
+    // no_4
+        // write  ('masukkan baris: ');readln(baris);
+        // write  ('masukkan kolom: ');readln(kolom);
+        // writeln('=====================');
+
+        // for a:=1 to baris do
+        //     begin
+        //         for b:=1 to kolom do
+        //             begin
+        //                 if ( a mod 2 = 0) then
+        //                     write(0,' ')
+        //                 else
+        //                     write(b,' ');
+        //             end;
+        //         writeln;
+        //     end;
+
+end.
